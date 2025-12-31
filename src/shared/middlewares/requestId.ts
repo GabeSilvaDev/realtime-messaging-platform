2c23f51cbc7c54b2c41e6bdf39789c1de1ecc0ce
GabeSilvaDev <spanxscomercial@gmail.com>
2025-12-31 00:30:23 -0300

    ✨ feat: Criando middleware de geração de Request ID

---
+++ src/shared/middlewares/requestId.ts
@@ -0,0 +1,39 @@
+import type { Request, Response, NextFunction } from 'express';
+import { v4 as uuidv4 } from 'uuid';
+import type { RequestIdOptions } from '../interfaces';
+import { REQUEST_ID_DEFAULT_HEADER_NAME } from '../constants';
+
+declare module 'express-serve-static-core' {
+  interface Request {
+    id: string;
+  }
+}
+
+export function createRequestIdMiddleware(options: RequestIdOptions = {}) {
+  const {
+    headerName = REQUEST_ID_DEFAULT_HEADER_NAME,
+    generator = uuidv4,
+    setResponseHeader = true,
+  } = options;
+
+  return function requestIdMiddleware(req: Request, res: Response, next: NextFunction): void {
+    const existingId = req.get(headerName);
+    const requestId = existingId ?? generator();
+
+    req.id = requestId;
+
+    if (existingId === undefined) {
+      req.headers[headerName] = requestId;
+    }
+
+    if (setResponseHeader) {
+      res.setHeader(headerName, requestId);
+    }
+
+    next();
+  };
+}
+
+export const requestIdMiddleware = createRequestIdMiddleware();
+
+export default requestIdMiddleware;
