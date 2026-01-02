1d2fd22b6103221ebee3969ebffae9cf24da979f
GabeSilvaDev <spanxscomercial@gmail.com>
2026-01-01 23:03:01 -0300

    🔧 chore: atualiza ESLint para ignorar arquivos de teste

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,12 @@
 import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
 import prettierConfig from 'eslint-config-prettier';
 import prettierPlugin from 'eslint-plugin-prettier';
 import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['node_modules/**', 'dist/**', '**/*.js', '**/*.mjs', '**/*.cjs'],
+    ignores: ['node_modules/**', 'dist/**', '**/*.js', '**/*.mjs', '**/*.cjs', 'jest.config.ts', 'tests/**'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
