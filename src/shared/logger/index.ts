b527ac25e8c0acf973bc6e4670d16ea5ff3debe5
GabeSilvaDev <spanxscomercial@gmail.com>
2026-01-06 22:48:08 -0300

    👌 style: Formata exports do logger em múltiplas linhas

--- src/shared/logger/index.ts
+++ src/shared/logger/index.ts
@@ -1,3 +1,10 @@
-export { Logger, initLogger, getLogger, isLoggerInitialized, LogLevel, LogCategory } from './Logger';
+export {
+  Logger,
+  initLogger,
+  getLogger,
+  isLoggerInitialized,
+  LogLevel,
+  LogCategory,
+} from './Logger';
 export type { ILogger, LogEntry, LogMetadata, LoggerOptions } from './Logger';
 export { LogModel } from './models/Log.model';
