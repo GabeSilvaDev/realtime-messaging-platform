61f844554947257d81800003efbd0008f36de602
GabeSilvaDev <spanxscomercial@gmail.com>
2026-09-24 02:52:09 -0300

    ✨ feat: recebe envio e confirmações de mensagens via Socket.IO com ack

---
+++ src/modules/realtime/handlers/index.ts
@@ -0,0 +1,4 @@
+export { withAck, toAckError } from './ack';
+export type { AckContext, AckListener } from './ack';
+export { registerMessageHandlers } from './messageHandlers';
+export type { MessageHandlerDeps } from './messageHandlers';
---
+++ tests/unit/modules/realtime/handlers/index.test.ts
@@ -0,0 +1,9 @@
+import * as handlers from '@/modules/realtime/handlers';
+
+describe('realtime handlers index', () => {
+  it('deve exportar o helper de ack e os registradores de handlers', () => {
+    expect(typeof handlers.withAck).toBe('function');
+    expect(typeof handlers.toAckError).toBe('function');
+    expect(typeof handlers.registerMessageHandlers).toBe('function');
+  });
+});
